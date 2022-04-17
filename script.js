80f2b87247983a64210582f20a3d4bd6df1c4fac
JP Moregain <j.p.moregain15@gmail.com>
2022-04-17 16:13:50 -0500

    Add calculator withsemi functional buttons

---
+++ script.js
@@ -0,0 +1,34 @@
+function sum(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case "+":
+            return sum(num1,num2);
+        case "-":
+            return subtract(num1,num2);
+        case "*":
+            return multiply(num1,num2);
+        case "/":
+            return divide(num1,num2);
+    }
+}
+let display = "";
+
+function press(x) {
+    document.getElementById('display').innerHTML = x;
+    display = x;
+}
